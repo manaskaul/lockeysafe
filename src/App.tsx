41427a014aa88d2ae170cd209c1450c79192cde0
kaul <kaulmanascocid@gmail.com>
2024-03-31 17:02:34 +0530

    wip: added signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Signup from "./components/signup/Signup";
 
 function App() {
-  
   return (
     <>
       <Navbar></Navbar>
+      <Signup></Signup>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
