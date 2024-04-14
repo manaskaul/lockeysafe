12ce43dacbbbe8b34dc8fe210138481df82ad336
kaul <kaulmanascocid@gmail.com>
2024-04-15 00:19:11 +0530

    wip: added breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="signup" element={<Signup />}></Route>
           <Route path="login" element={<Login />}></Route>
+          <Route path="link/:id" element={<Home />}></Route>
           <Route path="*" element={<Login />}></Route>
         </Routes>
       </div>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,67 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
+
+export interface Crumb {
+  pathText: string;
+  link: string;
+}
+
+function Breadcrumb() {
+  const [crumbs, setCrumbs] = useState([
+    {
+      pathText: "Home",
+      link: "/",
+    },
+  ]);
+
+  function addLinkToBreadcrumb() {
+    const newLink = {
+      pathText: `Link ${crumbs.length}`,
+      link: `/link/${crumbs.length}`,
+    };
+    setCrumbs((prevCrumb) => [...prevCrumb, newLink]);
+  }
+
+  function goBack(crumbLink: string) {
+    setCrumbs((crumbs) => {
+      const newCrumbs: Crumb[] = [...crumbs];
+      while (
+        newCrumbs.length > 0 &&
+        newCrumbs[newCrumbs.length - 1].link !== crumbLink
+      ) {
+        newCrumbs.pop();
+      }
+      return newCrumbs;
+    });
+  }
+
+  return (
+    <>
+      <div className="">
+        {crumbs.map((crumb: Crumb) => {
+          return (
+            <Link
+              to={crumb.link}
+              key={crumb.link}
+              onClick={() => goBack(crumb.link)}
+            >
+              {crumb.pathText}
+            </Link>
+          );
+        })}
+      </div>
+      <button onClick={() => addLinkToBreadcrumb()}>Add Crumb</button>
+    </>
+  );
+}
 
 function Home() {
   return (
     <div className="home">
-      <div className="shell"></div>
+      <div className="shell">
+        <Breadcrumb />
+      </div>
     </div>
   );
 }
